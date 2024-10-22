234b51a332741e0bab496acf23302a7232df18e6
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-22 19:00:16 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,30 @@
+a=29
+print("value of a:" ,a)
+print("Data type of a is:", type(a))
+a=str(a)
+print("value of a:" ,a)
+print("value of a:" ,a)
+print("Data type of a is:", type(a))
+
+b=9.6
+print("value of b:" ,b)
+print("Data type of b is:", type(b))
+b=int(b)
+print("value of b:" ,b)
+print("value of b:" ,b)
+print("Data type of b is:", type(b))
+
+c=False
+print("value of c:" ,c)
+print("Data type of c is:", type(c))
+c=str(c)
+print("value of c:" ,c)
+print("Data type of c is:", type(c))
+
+
+d="My name is Talish"
+print("value of d:" ,d)
+print("Data type of d is:", type(d))
+d=bool(d)
+print("value of d:" ,d)
+print("Data type of d is:", type(d))
